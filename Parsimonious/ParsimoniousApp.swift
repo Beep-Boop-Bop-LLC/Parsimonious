539a2e2fcf167c6a0e5088b544917041ce333291
Zach <venanziz@gmail.com>
2024-08-11 08:24:26 -0700

    Initial Commit

---
+++ Parsimonious/ParsimoniousApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParsimoniousApp.swift
+//  Parsimonious
+//
+//  Created by Zach Venanzi on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ParsimoniousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
